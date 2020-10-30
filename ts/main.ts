4a6167d1d6439c70d022192a9dea03a8227182bf
Joshua Westphalen <joshua.westphalen@yahoo.com>
2020-10-29 20:20:21 -0700

    main.ts continued: getVideoGame done

--- ts/main.ts
+++ ts/main.ts
@@ -32,10 +32,38 @@ function displayGame(myGame:VideoGame):void{
     //TODO: Display video game below the form
 }
 
+/**
+ * Gets all game data from the form 
+ * and returns it in a VideoGame object.
+ */
 function getVideoGame():VideoGame{
-    //TODO: Create game
-    //TODO: Populate with data from the form
-    //TODO: Return game
+    let game = new VideoGame();
+    //Populate with data from the form
+    let titleInput = 
+        <HTMLInputElement>document.getElementById("title");
+        game.title = titleInput.value;
+
+        let priceInput =
+            <HTMLInputElement>document.getElementById("price");
+        game.price = parseFloat(priceInput.value);
+
+        let ratingInput =
+            <HTMLInputElement>document.getElementById("rating");
+        game.rating = ratingInput.value;
+
+        let digitalOnly = 
+            <HTMLInputElement>document.getElementById("online");
+        //game.isDigitalOnly = digitalOnly.checked;
+        if(digitalOnly.checked){
+            game.isDigitalOnly = true;
+        }
+        else{
+            game.isDigitalOnly = false;
+        }
+
+    //Return game
+    return game;
+
 }
 
 function isAllDataValid(){
